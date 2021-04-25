8ba4899ee63525f195c987a2516cfdec2a755133
Eston Kagwima <ekagwima745@gmail.com>
2021-04-25 22:02:10 +0300

    creating  submit pitch form

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,7 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField
+from wtforms import StringField,TextAreaField,SubmitField,SelectField
 from wtforms.validators import Required
 
 class UpdateProfile(FlaskForm):
-    bio = TextAreaField('Tell us about you.',validators = [Required()])
-    submit = SubmitField('Submit')
+  bio = TextAreaField('Tell us about you.',validators = [Required()])
+  submit = SubmitField('Submit')
+
+class SubmitPitch(FlaskForm):
+  pitch_title = StringField('Title', validators = [Required()])
+  pitch_category= SelectField('Category', choices=[('Investor','Investor'),('Competition','Competition'),('Product','Product')],validators=[Required()])
+  pitch_post = TextAreaField ('Impress us!!',validators = [Required()])   
+  submit = SubmitField('Submit')  
