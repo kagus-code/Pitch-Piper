16d367a01cf76709fb975d6ace0d1b29249dc21c
Eston Kagwima <ekagwima745@gmail.com>
2021-04-25 16:27:56 +0300

    adding max width and height to image place holder

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
-from flask import render_template, redirect, url_for,abort
+from flask import render_template, request,redirect, url_for,abort
 from .forms import UpdateProfile
-from .. import db
+from .. import db,photos
 from ..models import User
 from . import main
 from flask_login import login_required
@@ -43,4 +43,16 @@ def update_profile(uname):
         return redirect(url_for('.profile',uname=user.username))
 
     return render_template('profile/update.html',form =form)
+
+
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))   
         
