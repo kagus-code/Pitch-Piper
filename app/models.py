0c4aa23446533748dd66d62eff5ae750199b970d
Eston Kagwima <ekagwima745@gmail.com>
2021-05-01 05:08:02 +0300

    adding upvote functionality

--- app/models.py
+++ app/models.py
@@ -17,7 +17,7 @@ class User(UserMixin, db.Model):
     profile_pic_path = db.Column(db.String(255))
     pitches = db.relationship('Pitch', backref='user', lazy='dynamic')
     comments = db.relationship('Comment', backref='user_comment', lazy='dynamic')
-
+    upvote = db.relationship('Upvote',backref='user',lazy='dynamic')
     @property
     def password(self):
         raise AttributeError('You cannot read the password attribute')
@@ -46,7 +46,7 @@ class Pitch(db.Model):
     posted = db.Column(db.DateTime,default=datetime.utcnow)
     user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
     comments = db.relationship('Comment', backref='user', lazy='dynamic')
-
+    upvote = db.relationship('Upvote',backref='pitch',lazy='dynamic')
     def save_pitch(self):
         db.session.add(self)
         db.session.commit()
@@ -76,6 +76,25 @@ class Comment(db.Model):
     def __repr__(self):
         return f'Comment:{self.comment}'    
 
-          
+class Upvote(db.Model):
+    __tablename__ = 'upvotes'
+
+    id = db.Column(db.Integer,primary_key=True)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    pitch_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
+    
+
+    def add_upvote(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_upvotes(cls,id):
+        upvote = Upvote.query.filter_by(pitch_id=id).all()
+        return upvote
+
+
+    def __repr__(self):
+        return f'{self.user_id}:{self.pitch_id}'          
 
 
